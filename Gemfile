d6da67cf91538727066e56d1cf36c878b5030b70
key0123456789 <k.i-0123456789@live.jp>
2019-11-29 22:34:35 +0900

    mailer settings

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'pry-rails' 
   gem 'better_errors'  
+  gem 'letter_opener_web', '~> 1.0'
 end
 
 group :test do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # 本番環境でマスターキーが指定されていないときにエラーを出す
+  config.require_master_key = true
 end
