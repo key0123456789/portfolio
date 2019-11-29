170c19b2bfe79b9b17f895d6db0a0271933bb62e
key0123456789 <k.i-0123456789@live.jp>
2019-11-29 22:38:55 +0900

    Create views(temporary)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root "main#index"
 
-  get '', to: "main#index"
+  get  '', to: "main#index"
+  
+  get  'index' =>'messages#index'
+  post 'confirm' => 'messages#confirm'
+  post 'done' => 'messages#done'
+
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
