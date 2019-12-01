9e09ebb93f5ae757db29b9a49e3dd035502c7e11
key0123456789 <k.i-0123456789@live.jp>
2019-12-02 00:28:13 +0900

    Add mailer setting to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,16 @@ Rails.application.configure do
 
   # 本番環境でマスターキーが指定されていないときにエラーを出す
   config.require_master_key = true
+
+  #mailer
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "gmail.com",
+      user_name: ENV['SENDGRID_USERNAME'],
+      password: ENV['SENDGRID_PASSWORD'],
+      authentication: :plain,
+      enable_starttls_auto: true
+  }
 end
