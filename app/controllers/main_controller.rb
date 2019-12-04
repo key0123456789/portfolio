9e61bd7101e183baf9b00acf71f6c8faf7f64318
key0123456789 <k.i-0123456789@live.jp>
2019-12-04 23:23:34 +0900

    Setting EV

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,10 +5,15 @@ class MainController < ApplicationController
 
   def done
     @message = Message.new(message_params)
-    if params[:back]
+    if @message.valid?
       render action: 'index'
     else
       MessageMailer.received_email(@message).deliver_now
     end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:name, :email, :content)
   end
 end
